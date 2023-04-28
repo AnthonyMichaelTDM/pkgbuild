7bfddd9bb5866c51d176e772808c74cae14cc1e4
AnthonyMichaelTDM <68485672+AnthonyMichaelTDM@users.noreply.github.com>
2023-04-27 22:09:53 -0700

    updates

--- edu-nvim-meta/init.lua
+++ edu-nvim-meta/init.lua
@@ -191,6 +191,22 @@ local config = {
   },
   -- Configure plugins
   plugins = {
+    {
+      "zbirenbaum/copilot.lua",
+      cmd = "Copilot",
+      event = "InsertEnter",
+      config = function()
+        require("copilot").setup({
+          panel = {
+            auto_refresh = true,
+          },
+          suggestion = {
+            auto_trigger = true,
+          }
+        })
+      end,
+    },
+
     -- rust support
     {
       "williamboman/mason-lspconfig.nvim",
